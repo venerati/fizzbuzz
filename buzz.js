4ed13aa4b11f1e920ea660ad38537a7a633fee81
James Marshall <jamesomarshall@gmail.com>
2017-06-30 12:15:02 -0400

    finished putting the logic into the function. I moved the divisavle by 3 and 5 argument to the top so that there would be no conflict from the divisable by 3 statement.

--- buzz.js
+++ buzz.js
@@ -1,9 +1,17 @@
 function buzz() {
  for(i=1; i < 101; i++) {
-  console.log(i);
-  if(i % 3 === 0) {
-   console.log("its divisable by 3");
-  }
+  if(i % 15 === 0) 
+   console.log("it is divisable by both 3 and 5");
+
+  else if(i % 5 === 0) 
+   console.log("its divisable by 5");
+  
+  else if(i % 3 === 0) 
+   console.log("it is divisable by 3");
+  
+  else 
+   console.log(i);
+  
  }
 }
 
