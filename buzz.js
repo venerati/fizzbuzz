30ac7d61772971b878397d41366b49902ddb1d79
James Marshall <jamesomarshall@gmail.com>
2017-07-03 13:17:43 -0400

    took the value that the var i looped against and turned it into a variable that was controled by user inpur. also bound the functions to a button that would run on click. Made it so that the user saw a feed back of how many times fizz and buzz occured within the number provided.

--- buzz.js
+++ buzz.js
@@ -2,27 +2,29 @@
 var fizz = 0;
 
 function buzz() {
- 
+ //takes the number that the user puts in the input field and binds it to x
  var x = document.getElementById('fizzNumber').value;
 
  for(i=1; i < x; i++) {
   
   if(i % 15 === 0) {
-   document.getElementById('fuzzy').innerHTML += "FizzBuzz";
+   //document.getElementById('fuzzy').innerHTML += "FizzBuzz";
    fizz++;
   }
 
   else if(i % 5 === 0) {
-   document.getElementById('fuzzy').innerHTML += "Buzz";
+   //document.getElementById('fuzzy').innerHTML += "Buzz";
    fizz++; 
   }
   else if(i % 3 === 0) {
-   document.getElementById('fuzzy').innerHTML += "Fizz";
+   //document.getElementById('fuzzy').innerHTML += "Fizz";
    fizz++;
   }
    
-  else 
-   document.getElementById('fuzzy').innerHTML += i;
-  
+  else {
+   //document.getElementById('fuzzy').innerHTML += i;
+  }
  }
+ document.getElementById('tally').innerHTML = '<h1>You have ' + fizz + ' Fizz power in your number</h1>';
+
 }
